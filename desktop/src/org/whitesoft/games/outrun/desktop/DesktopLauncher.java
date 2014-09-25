ea55af2a8b7fbbb4793501eb42d5367c6ea9edee
Christoph Weiß <christoph.weiss@gmail.com>
2014-09-25 11:55:38 +0200

    Initial commit

---
+++ desktop/src/org/whitesoft/games/outrun/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.whitesoft.games.outrun.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.whitesoft.games.outrun.GdxOutrunGame;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new GdxOutrunGame(), config);
+	}
+}
